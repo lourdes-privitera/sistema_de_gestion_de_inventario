1c68036834bf686614002bf9157c9d82b74f0fb6
lourdes-privitera <lulitaprivi@gmail.com>
2026-07-08 19:44:55 -0300

    feat(5. Modificar producto /6. Eliminar producto ): implementar logica de modificacion de productos con sus validaciones y agregar funcion de eliminacion

--- productos.py
+++ productos.py
@@ -1,11 +1,19 @@
 from inputs import (
-    pedir_informacion_producto
+    pedir_informacion_producto,
+    ingresar_nombre,
+    ingresar_categoria,
+    ingresar_precio,
+    ingresar_stock_disponible,
+    ingresar_stock_minimo,
+    ingresar_proveedor
 )
 
 from output import(
     mostrar_producto
 )
-
+from utilidades import(
+    buscar_producto
+)
 
 def registrar_producto(inventario:dict) -> dict:
     """Registra un nuevo producto en el inventario.
@@ -61,5 +69,58 @@ def buscar_productos_por_categoria(inventario:dict, categoria:str) -> dict:
 
     return productos_encontrados
 
+def modificar_producto(inventario:dict) -> None:
+    """Modifica los datos de un producto existente.
 
+    Solicita el código del producto, verifica que exista
+    y permite actualizar todos sus datos.
 
+    Args:
+        inventario (dict): Diccionario que contiene los productos.
+    """
+
+    codigo = input("Ingrese el código del producto: ")
+
+    producto = buscar_producto(inventario, codigo)
+
+    if producto != None:
+
+        producto.update({
+            "nombre": ingresar_nombre(),
+            "categoria": ingresar_categoria(),
+            "precio": ingresar_precio(),
+            "stock_disponible": ingresar_stock_disponible(),
+            "stock_minimo": ingresar_stock_minimo(),
+            "proveedor": ingresar_proveedor()
+        })
+
+        print("Producto modificado correctamente.")
+
+    else:
+        print("Producto inexistente.")
+
+def eliminar_producto(inventario:dict) -> None:
+    """Elimina un producto del inventario.
+
+    Solicita el código del producto y, si existe,
+    pide confirmación antes de eliminarlo.
+
+    Args:
+        inventario (dict): Diccionario que contiene los productos.
+    """
+
+    codigo = input("Ingrese el código del producto: ")
+
+    if codigo in inventario:
+
+        confirmar = input("¿Eliminar producto? (s/n): ")
+
+        if confirmar == "s" or confirmar == "S" :
+
+            inventario.pop(codigo)
+
+            print("Producto eliminado.")
+
+    else:
+
+        print("Producto inexistente.")
